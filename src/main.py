615b2eba5d30348fc046b6b080e3183f97f9e8c4
Thiago Silva Murta <thiagosmurta@hotmail.com>
2023-11-19 14:11:31 -0300

    adicionei o main e as bibliotecas

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import sympy as sy
+from sympy import init_printing
+init_printing(use_latex='png', scale=1.05, order='grlex', forecolor='Black', backcolor='White', fontsize=10)
+from sympy import diff, Symbol
+from sympy import sin, cos, exp
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as sc
+import scipy.io
+from scipy.linalg import lu
+import scipy.sparse.linalg
+
